fe97eba5733cec8ba808535bdf0a33613bf7093a
Vikram Kumar <vikramkumarhbti92@gmail.com>
2025-09-14 12:58:17 +0530

    Initial Commit

---
+++ BLEConnect/BLEConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEConnectApp.swift
+//  BLEConnect
+//
+//  Created by Vikram Kumar on 14/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BLEConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
